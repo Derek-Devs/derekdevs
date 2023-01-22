bd6295e2518a39776d7a166786b1e3984cd65fa4
Derek_Devs <derekwshirley@gmail.com>
2023-01-21 22:29:37 -0600

    first commit

---
+++ derekDevs/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div class="navbar bg-base-100">
+    <a class="btn btn-ghost normal-case text-xl" href="+page.svelte">DerekDevs</a>
+    <select data-choose-theme>
+      <option value="">Default</option>
+      <option value="dark">Dark</option>
+      <option value="pink">Pink</option>
+    </select>
+</div>
+<div class="bg-gradient-to-t from-base-100 p-5">
+      <div class="mockup-code w-1/2 mx-auto max-w-s">
+        <pre data-prefix="$"><code>pnpm i addDerekToTeam</code></pre> 
+        <pre data-prefix=">" class="text-warning"><code>installing...</code></pre> 
+        <pre data-prefix=">" class="text-success"><code>Done!</code></pre>
+  </div>
+</div>
+    <div class="indicator">
+      <div class="indicator-item indicator-bottom">
+        <button class="btn btn-primary">Apply</button>
+      </div> 
+      <div class="card border">
+        <div class="card-body">
+          <h2 class="card-title">Job Title</h2> 
+          <p>Rerum reiciendis beatae tenetur excepturi</p>
+        </div>
+      </div>
+    </div>
---
+++ derekDevs/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { onMount } from 'svelte'
+import { themeChange } from 'theme-change'
+
+/** @type {import('@sveltejs/kit').Config} */
+onMount(() => {
+	themeChange(false)
+	// 👆 false parameter is required for svelte
+  })
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
+};
+export default config;
