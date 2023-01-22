e740978692b9615e5a0a65f39795586f2b1649f4
Derek_Devs <derekwshirley@gmail.com>
2023-01-22 14:08:28 -0600

    Added some changes to the light/dark button.

--- derekDevs/src/routes/+layout.svelte
+++ derekDevs/src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
     import "../app.css";
   </script>
-  
+    <div class="navbar bg-base-100">
+      <div class="flex-1">
+      <a class="btn btn-ghost normal-case text-xl" href="/routes">DerekDevs</a>
+    </div>
+    <div class="flex-none">
+      <label class="swap swap-flip text-xl justify-end">
+        <!-- this hidden checkbox controls the state -->
+        <input type="checkbox" />
+        <div class="swap-on">🌞</div>
+        <div class="swap-off">🌑</div>
+      </label>
+    </div>
+  </div>
   <slot />
--- derekDevs/src/routes/+page.svelte
+++ derekDevs/src/routes/+page.svelte
@@ -1,12 +1,5 @@
-<div class="navbar bg-base-100">
-    <a class="btn btn-ghost normal-case text-xl" href="+page.svelte">DerekDevs</a>
-    <select data-choose-theme>
-      <option value="">Default</option>
-      <option value="dark">Dark</option>
-      <option value="pink">Pink</option>
-    </select>
-</div>
-<div class="bg-gradient-to-t p-5">
+
+<div class="container mx-auto bg-repeat text-primary-100 heropattern-topography-inherit p-5 rounded-md">
       <div class="mockup-code w-1/2 mx-auto max-w-s">
         <pre data-prefix="$"><code>pnpm i addDerekToTeam</code></pre> 
         <pre data-prefix=">" class="text-warning"><code>installing...</code></pre> 
@@ -22,12 +15,5 @@
           <h2 class="card-title">Job Title</h2> 
           <p>Rerum reiciendis beatae tenetur excepturi</p>
         </div>
-      </div>
-    </div>
-    <div class="flex m-2 relative w-40 h-40 sm:pb-0 sm:w-48 sm:h-48 bg-red-500">
-      <div
-        class="bg-repeat w-full h-full text-primary-100 heropattern-jigsaw-red-100"
-      >
-        <div class="text-sm inline">heropattern-jigsaw-red-500</div>
       </div>
     </div>
