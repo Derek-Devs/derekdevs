3670682edd304ded8b8828767efa053e278cf488
Derek_Devs <derekwshirley@gmail.com>
2023-01-26 17:36:02 -0600

    Building for production 2.0

---
+++ derekDevs/.netlify/server/entries/pages/_layout.svelte.js
@@ -0,0 +1,22 @@
+import { c as create_ssr_component } from "../../chunks/index.js";
+import "theme-change";
+const app = "";
+const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
+  return `<div class="${"sticky top-0 z-50 navbar bg-base-inherit bg-opacity-0 heropattern-topography-inherit rounded-sm backdrop-blur"}"><div class="${"flex-1 space-x-5"}"><a class="${"btn btn-primary normal-case text-xl"}" href="${"/"}">Home</a>
+      <a class="${"btn btn-primary normal-case text-xl"}" href="${"/contactForm"}">Contact</a></div>
+    <div class="${"flex-none"}"><div class="${"invisible lg:visible xl:visible md:visible tooltip tooltip-open tooltip-left"}" data-tip="${"Click me to change the website theme!"}"></div>
+        <label class="${"swap swap-flip text-3xl justify-end"}">
+          <input type="${"checkbox"}" data-toggle-theme="${"night,light"}" data-act-class="${"ACTIVECLASS"}">
+          <div class="${"swap-on"}">🌞</div>
+          <div class="${"swap-off"}">🌜</div></label></div></div>
+  ${slots.default ? slots.default({}) : ``}
+  
+  <footer class="${"bottom-0 z-50 footer items-center p-4 bg-neutral text-neutral-content"}"><div class="${"items-center grid-flow-col"}"><svg href="${"/contactForm"}" xmlns="${"http://www.w3.org/2000/svg"}" fill="${"none"}" viewBox="${"0 0 24 24"}" stroke-width="${"1.5"}" stroke="${"currentColor"}" class="${"w-6 h-6"}"><path stroke-linecap="${"round"}" stroke-linejoin="${"round"}" d="${"M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"}"></path></svg>
+    <p><a class="${"link"}" href="${"/contactForm"}">Click here to send me a message!</a></p></div> 
+    <div class="${"grid-flow-col gap-4 md:place-self-center md:justify-self-end"}"><a href="${"https://twitter.com/Derek_Devs"}"><svg xmlns="${"http://www.w3.org/2000/svg"}" width="${"24"}" height="${"24"}" viewBox="${"0 0 24 24"}" class="${"fill-current"}"><path d="${"M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"}"></path></svg></a> 
+      <a href="${"https://www.linkedin.com/in/derekdevs/"}"><svg xmlns="${"http://www.w3.org/2000/svg"}" width="${"24"}" height="${"24"}" viewBox="${"0 0 24 24"}" class="${"fill-current"}"><path d="${"M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"}"></path></svg></a>
+      <a href="${"https://github.com/Derek-Devs"}"><svg xmlns="${"http://www.w3.org/2000/svg"}" width="${"24"}" height="${"24"}" viewBox="${"0 0 24 24"}" class="${"fill-current"}"><path d="${"M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"}"></path></svg></a></div></footer>`;
+});
+export {
+  Layout as default
+};
---
+++ derekDevs/.netlify/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+export const component = async () => (await import('../entries/pages/_error.svelte.js')).default;
+export const file = '_app/immutable/components/pages/_error.svelte-a13416ff.js';
+export const imports = ["_app/immutable/components/pages/_error.svelte-a13416ff.js","_app/immutable/chunks/index-4f11b2f3.js"];
+export const stylesheets = [];
+export const fonts = [];
