781ad96063719ab58930d278d25a1cd069df84ae
Derek_Devs <derekwshirley@gmail.com>
2023-01-26 21:43:34 -0600

    Netlify attempt.

---
+++ derekDevs/.netlify/server/nodes/3.js
@@ -0,0 +1,8 @@
+
+
+export const index = 3;
+export const component = async () => (await import('../entries/pages/contactForm/_page.svelte.js')).default;
+export const file = '_app/immutable/components/pages/contactForm/_page.svelte-3d91363e.js';
+export const imports = ["_app/immutable/components/pages/contactForm/_page.svelte-3d91363e.js","_app/immutable/chunks/index-4f11b2f3.js"];
+export const stylesheets = [];
+export const fonts = [];
