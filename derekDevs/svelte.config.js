65e1bc7a9c6329880c9b3ca1662a1c88eff9d10b
Derek_Devs <derekwshirley@gmail.com>
2023-01-26 21:38:44 -0600

    Deployment

--- derekDevs/svelte.config.js
+++ derekDevs/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { onMount } from 'svelte'
 import { themeChange } from 'theme-change'
@@ -10,7 +10,16 @@ onMount(() => {
   })
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+ 
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    })
   },
   preprocess: vitePreprocess()
 };
