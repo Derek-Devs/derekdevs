f2821e5ee2cda1a2b965df8281cdf09e3514211b
Derek_Devs <derek@derekdevs.com>
2023-03-06 16:24:18 -0600

    Complete website overhaul. Added pages for Data analytics and Web Development. Added a page for personal projects. Added several graphics. Added dropdown menu.

--- derekDevs/src/routes/+page.svelte
+++ derekDevs/src/routes/+page.svelte
@@ -1,32 +1,11 @@
 <script lang="ts">
 // @ts-nocheck
-  const openSourceLibraryPic = new URL('images/heropatterns.PNG', import.meta.url).href
-  const unarchivedClothingPic = new URL('images/website.PNG', import.meta.url).href
-  const profilePic = new URL('images/pfp.png', import.meta.url).href
-  const portfolioPic = new URL('images/screenshotcontact.PNG', import.meta.url).href
-  const artistPic = new URL('images/artist.png', import.meta.url).href
-
-// import { request, gql } from 'graphql-request'
-
-// const query = gql`
-// {
-//   username {
-//     userProblemsSolved
-//     allQuestionsCount
-//     problemsSolvedBeatsStats
-//     difficulty
-//     percentage
-//     itStatsGlobal
-//     acSubmissionsNum
-//   }
-//   variables: {
-//     username: DerekDevs
-//   }
-// }
-// `
-// request('https://leetcode.com/graphql', query).then((data) => console.log(data))
-
+  const artistPic = new URL('/static/images/artist.png', import.meta.url).href
+  const webDev = new URL('/static/images/webdev.png', import.meta.url).href
+  const extras = new URL('/static/images/extras.png', import.meta.url).href
+  const data = new URL('/static/images/data.png', import.meta.url).href
 </script>
+
 <svelte:head>
     <title>Home</title> 
 </svelte:head>
@@ -37,118 +16,48 @@
   <div class="hero-content flex-col lg:flex-row">
     <img src="{artistPic}" alt="A man sitting at a desk painting near a window" class="max-w-sm rounded-lg shadow-2xl" />
       <div>
-        <h1 class="text-5xl font-bold">Welcome to DerekDevs.com</h1>
-        <p class="py-6">My name is Derek. Scroll down to view my projects, or click the button below to get my contact info.</p>
-        <button class="btn btn-primary" onclick="location.href='/contactForm';">Get Started</button>
+        <h1 class="text-5xl font-bold bg-gradient-to-tr from-primary to-secondary bg-clip-text">Welcome to DerekDevs.com</h1>
+        <p class="py-6">My name is Derek. Scroll down to select a portfolio, or click the button below to get my contact info.</p>
+        <button class="btn btn-primary" onclick="location.href='/contactForm';">Contact Information</button>
       </div>
   </div>
 </div>
 
-
-<!-- <div class="grid-cols-2 grid-row-1 grid-flow-row-dense">
-  <div class="p-5 rounded">
-    <div class="hero min-h-screen p-5 rounded-md">
-      <figure><img src="{artistPic}" alt="A man with curly hair creates artwork in a bedroom at night." /></figure>
-      <div class="hero-overlay bg-opacity-60 rounded"></div>
-      <div class="hero-content text-center text-neutral-content rounded">
-        <div class="max-w-md rounded">
-          <h1 class="mb-5 text-5xl font-bold">Welcome to my Portfolio Website</h1>
-          <p class="mb-5">My name is Derek. Scroll down to view my projects, or click the button below to read more about me and get my contact info.</p>
-          <button class="btn btn-primary">Contact.</button>
-        </div>
-      </div>
-    </div>
-  </div>
-    <div class="p-5 rounded">
-    <div class="hero min-h-screen p-5 rounded-md">
-      <figure><img src="{artistPic}" alt="A man with curly hair creates artwork in a bedroom at night." /></figure>
-      <div class="hero-overlay bg-opacity-60 rounded"></div>
-      <div class="hero-content text-center text-neutral-content rounded">
-        <div class="max-w-md rounded">
-          <h1 class="mb-5 text-5xl font-bold">Welcome to my Portfolio Website</h1>
-          <p class="mb-5">My name is Derek. Scroll down to view my projects, or click the button below to read more about me and get my contact info.</p>
-          <button class="btn btn-primary">Contact.</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div> -->
-<div class="divider p-5">Portfolio</div>
+<div class="divider p-5">Portfolio Overview</div>
 
 <div class="grid sm:grid-col-1 md:grid-col-2 lg:grid-cols-3 sm:grid-row-1 md:grid-row-2 lg:grid-row-3 p-2 gap-4">
 <!-- Portfolio Section-->
   <div class="card w-full bg-base-100 shadow-xl image-full card-body">
-    <figure><img src="{portfolioPic}" alt="A screenshot of a portfolio website." /></figure>
+    <figure><img src="{webDev}" alt="A man with curly hair programming on a computer." /></figure>
     <div class="card-body">
-      <h2 class="card-title justify-center">Open Development Portfolio Website</h2>
-      <p>Developed an entirely open source portfolio website using Javascript, Typescript, CSS, HTML, Sveltekit, Vite, NodeJS and other technologies.</p>
+      <h2 class="card-title justify-center">Web Development Portfolio</h2>
+      <p>Take a look at examples of full-stack devlelopment projects I have created.</p>
       <div class="card-actions justify-end">
-        <button class="btn btn-primary" onclick="location.href='https://github.com/Derek-Devs/derekdevs';">View the repository</button>
+        <button class="btn btn-primary" onclick="location.href='/webDevelopment';">View</button>
       </div>
     </div>
   </div>
   <div class="card w-full bg-base-100 shadow-xl image-full card-body">
-    <figure><img src="{openSourceLibraryPic}" alt="A screenshot of a CSS pattern framework plugin." /></figure>
+    <figure><img src="{data}" alt="A screenshot of a CSS pattern framework plugin." /></figure>
     <div class="card-body">
-      <h2 class="card-title justify-center">Tailwind CSS Library</h2>
-      <p>Contributed to an Open Source Tailwind CSS Library for using the Hero Patterns you see on this website</p>
+      <h2 class="card-title justify-center">Data Analysis Portfolio</h2>
+      <p>Projects and tools developed to analyze, visualize, or manipulate data.</p>
       <div class="card-actions justify-end">
-        <button class="btn btn-primary" onclick="location.href='https://github.com/svengau/tailwindcss-hero-patterns/pull/5';">View Pull Request</button>
+        <button class="btn btn-primary" onclick="location.href='/dataAnalytics';">View</button>
       </div>
     </div>
   </div>
   <div class="card w-full bg-base-100 shadow-xl image-full card-body">
-    <figure><img src="{unarchivedClothingPic}" alt="A screenshot of a clothing webstore." /></figure>
+    <figure><img src="{extras}" alt="A wizard stands in the midst of falling multiclored books." /></figure>
     <div class="card-body">
-      <h2 class="card-title justify-center">Clothing Webstore</h2>
-      <p>Created a webstore for a local clothing brand built using a popular CMS and minimalist design features. Also utilized digital design software such as Photoshop.</p>
+      <h2 class="card-title justify-center">Extras</h2>
+      <p>Programming and data related personal projects pertaining to my hobbies and interests</p>
       <div class="card-actions justify-end">
-        <button class="btn btn-primary" onclick="location.href='https://www.unarchived.us/';">Check it out</button>
+        <button class="btn btn-primary" onclick="location.href='/extras';">View</button>
       </div>
     </div>
   </div>
 </div>
-
-<!-- <div class="divider">Statistics</div> -->
-
-<!-- Stats Section-->
-<!-- <div class="p-2">
-<div class="stats shadow-xl stats-vertical lg:stats-horizontal p-2 grid">
-  <div class="stat">
-    <div class="stat-figure text-primary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
-      </svg>
-          </div>
-    <div class="stat-title">Leetcode Problems Solved</div>
-    <div class="stat-value text-primary">25.6K</div>
-    <div class="stat-desc">21% more than last month</div>
-  </div>
-  
-  <div class="stat">
-    <div class="stat-figure text-secondary">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-    </div>
-    <div class="stat-title">Page Views</div>
-    <div class="stat-value text-secondary">2.6M</div>
-    <div class="stat-desc">21% more than last month</div>
-  </div>
-  
-  <div class="stat">
-    <div class="stat-figure text-secondary">
-      <div class="avatar online">
-        <div class="w-16 rounded-full">
-          <img src="{profilePic}" alt="The website owner in a suit."/>
-        </div>
-      </div>
-    </div>
-    <div class="stat-value">86%</div>
-    <div class="stat-title">Tasks done</div>
-    <div class="stat-desc text-secondary">31 tasks remaining</div>
-  </div>
-</div>
-</div> -->
 
 <div class="divider p-5">Want to get in touch?</div>
 
---
+++ derekDevs/src/routes/extras/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  const underConstruction = new URL('/static/images/underconstruction.png', import.meta.url).href
+</script>
+
+<div class="divider p-2">Extras Overview</div>
+
+<div class="p-5 rounded-lg">
+    <div class="hero-content flex-col lg:flex-row">
+      <img src="{underConstruction}" alt="A group of boys working on a piece of machinery surrounded by candles" class="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 class="text-5xl font-bold bg-gradient-to-tr from-primary to-secondary bg-clip-text">Under Construction</h1>
+          <p class="py-6">I am still adding projects to this section, please check back later!</p>
+          <button class="btn btn-primary" onclick="location.href='/';">Home</button>
+        </div>
+    </div>
+  </div>
+
+<div class="grid sm:grid-col-1 md:grid-col-2 lg:grid-cols-3 sm:grid-row-1 md:grid-row-2 lg:grid-row-3 p-2 gap-4">
+
+</div>
+
